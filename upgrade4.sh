d4babbb8319a41e40c8274ab4ef105cf0705d1e9
Christine Tham <chris.tham@hellotham.com>
2021-10-15 19:46:44 +1100

    Upgrade to gatsby v4

--- upgrade4.sh
+++ upgrade4.sh
@@ -3,11 +3,13 @@ yarn add gatsby@next \
     gatsby-plugin-manifest@next \
     gatsby-plugin-mdx@next \
     gatsby-plugin-offline@next \
-    gatsby-plugin-page-creator@next \
+    gatsby-plugin-gatsby-cloud@next \
     gatsby-plugin-react-helmet@next \
     gatsby-plugin-sharp@next \
     gatsby-remark-copy-linked-files@next \
     gatsby-remark-images@next \
     gatsby-remark-smartypants@next \
     gatsby-source-filesystem@next \
-    gatsby-transformer-sharp@next
+    gatsby-transformer-sharp@next \
+    gatsby-plugin-sitemap@next
+yarn add -D gatsby-plugin-postcss@next
